de7d7e1d97ddf6d57054e34753d54b5641284047
Carlisia Campos <carlisia@grokkingtech.io>
2014-03-06 10:01:21 -0800

    To be, or not to be because, awesome.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geolocationapp::Application.config.secret_key_base = '0ffe870c433b38dca6413ba739ce2f5ea20306a925f1992fc76c21d4d68ab9d6309f414b693618499efb90f50c67c8cde2ca46fbb7a7aa264b25b574aff697a5'
