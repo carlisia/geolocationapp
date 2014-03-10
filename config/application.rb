cc15f8a482fa32cad42358297fd6ec8f923e8a06
Carlisia Campos <carlisia@grokkingtech.io>
2014-03-10 00:17:50 -0700

    Ow postgis...

--- config/application.rb
+++ config/application.rb
@@ -12,28 +12,6 @@ require 'active_record/connection_adapters/postgis_adapter/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-class ActiveRecordOverrideRailtie < Rails::Railtie
-  initializer "active_record.initialize_database.override" do |app|
-
-    ActiveSupport.on_load(:active_record) do
-      if url = ENV['DATABASE_URL']
-        ActiveRecord::Base.connection_pool.disconnect!
-        parsed_url = URI.parse(url)
-        config =  {
-          adapter:             'postgis',
-          host:                parsed_url.host,
-          encoding:            'unicode',
-          database:            parsed_url.path.split("/")[-1],
-          port:                parsed_url.port,
-          username:            parsed_url.user,
-          password:            parsed_url.password
-        }
-        establish_connection(config)
-      end
-    end
-  end
-end
-
 module Geolocationapp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -47,6 +25,11 @@ module Geolocationapp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    def config.database_configuration
+      parsed = super
+      parsed.each_value { |config| config['adapter'] = 'postgis' }
+    end
     
     config.autoload_paths += %W(#{config.root}/lib)
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
